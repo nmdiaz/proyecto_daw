880cf82cbf93b5392cc20d66227b3712ad8ba933
nmaria <nestor.md89@gmail.com>
2018-05-17 18:46:23 +0200

    added-save-recorder-not-working

---
+++ proyecto_final_moha_nestor/Models/RecordModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace proyecto_final_moha_nestor.Models
+{
+    public class recordModel
+    {
+        [Required]
+        public string idUser { get; set; }
+
+        [Required]
+        public string idGame { get; set; }
+
+        [Required]
+        public TimestampAttribute time { get; set; } 
+
+        [Required]
+        public int score { get; set; }
+    }
+
+}
---
+++ proyecto_final_moha_nestor/logic/DataSaver.cs
@@ -0,0 +1,36 @@
+﻿using proyecto_final_moha_nestor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyecto_final_moha_nestor.logic
+{
+    public class DataSaver
+    {
+        public bool SaveData(recordModel model)
+        {
+            var conetionString = "Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\aspnet-proyecto_final_moha_nestor-20180515073549.mdf;Initial Catalog=aspnet-proyecto_final_moha_nestor-20180515073549;Integrated Security=True";
+            try
+            {
+                System.Data.SqlClient.SqlConnection sqlConnection1 =
+                new System.Data.SqlClient.SqlConnection(conetionString);
+                System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = $"INSERT Records (IdGame, IdUser, RecordTimeStamp, Points) VALUES ({model.idGame}, {model.idUser}, {model.time},{model.score})";
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+                cmd.ExecuteNonQuery();
+                sqlConnection1.Close();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
