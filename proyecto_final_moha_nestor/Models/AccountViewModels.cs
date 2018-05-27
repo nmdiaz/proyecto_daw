d9adf28bfda7ef769c277f4b1dbef057f42f2925
nmaria <nestor.md89@gmail.com>
2018-05-27 17:57:10 +0200

    fix login

--- proyecto_final_moha_nestor/Models/AccountViewModels.cs
+++ proyecto_final_moha_nestor/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace proyecto_final_moha_nestor.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
