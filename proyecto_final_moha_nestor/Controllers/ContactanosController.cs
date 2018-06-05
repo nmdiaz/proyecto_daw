ab045f76174b6f19688f4e1064f6f477dd9b404e
Mohaloubani <mohammadloubani@gmail.com>
2018-06-05 15:53:37 +0200

    Añadida pestaña y controlador de "contactanos" basica

---
+++ proyecto_final_moha_nestor/Controllers/ContactanosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto_final_moha_nestor.Controllers
+{
+    public class ContactanosController : Controller
+    {
+        // GET: Contactanos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
