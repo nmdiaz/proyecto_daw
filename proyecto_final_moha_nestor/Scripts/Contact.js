f2ec8b34a6b1db8e845b87abcb2fdbabeabe5b34
Mohaloubani <mohammadloubani@gmail.com>
2018-06-06 11:47:13 +0200

    Añadido formulario en la pestaña "contactanos"

---
+++ proyecto_final_moha_nestor/Scripts/Contact.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+let elForm = document.forms["form"];
+let ctrlEm = elForm.elements["email_usuario"];
+ctrlEm.addEventListener("change", function (e) {
+    let control = this;
+    let expReg = new RegExp(control.pattern);
+    control.nextElementSibling.innerHTML = "";
+    if (!expReg.test(control.value)) {
+        control.focus();
+        control.nextElementSibling.innerHTML = "Correo inválido";
+        document.getElementsByTagName('span')[0].style.color = 'red';
+    }
+
+});
